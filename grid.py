60fba26e2f65c0f2e671dc9d3f18bbb791a1ac3c
Mustafa Vora <184190001@iitb.ac.in>
2020-09-20 19:00:18 +0530

    new grid

--- grid.py
+++ grid.py
@@ -1,23 +1,46 @@
 # funciton to get grid of the fi
 import matplotlib.pyplot as plt
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.inRange = [] #this will contain all the points that are within 400m from this point
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def check_constraints(self, pool):
+       for x in self.inRange:
+           if x in pool:
+               return False, pool
+       else:
+           return True, (pool+self.point)
+           
+def checkCircle(centre, point, radius=400):
+    return (point.x - centre.x)**2 + (point.y - centre.y)**2 < radius**2
+
+def fillNeighbours(points):
+    for p1 in points:
+        for p2 in points:
+            if p2 != p1:
+                if ((p1.x - 400 < p2.x < p1.x + 400) and (p1.y - 400 < p2.y < p1.y + 400)):
+                    if(checkCircle(p1,p2)):
+                        p1.inRange.append(p2)
+       
 def grid(n):  # n is the number of divisions you are making. 
-    x=[]
-    y=[]
+    points = []
     div_len = 4000/n
     for i in range(0,n):
         if (i*div_len) >3950 or (i*div_len) <50:
             continue
         else:
             for j in range(0,n):
-                if (j*div_len) >3950 or (j*div_len) <50:
+                if (j*div_len) >3950 or (j*div_len) <50: 
                     continue
                 else:
-                    x.append(i*div_len)
-                    y.append(j*div_len)
+                    points.append(Point(i*div_len, j*div_len))
+                    
+    fillNeighbours(points)
     
-    return tuple(zip(x,y))
-
-grid_points = grid(50)
-plt.scatter(*zip(*grid_points))
-plt.show()
+    return points
